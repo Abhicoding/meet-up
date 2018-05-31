f2cfd50d57862dfdfad063f84c57cdc3b0d4e5d4
Abhicoding <shinganeabhishek@gmail.com>
2018-05-31 08:14:40 +0530

    Able to redirect to github auth page

--- src/components/githubOauth/githubOauth.js
+++ src/components/githubOauth/githubOauth.js
@@ -3,27 +3,27 @@ import React from 'react'
 class GithubOauth extends React.Component {
   constructor () {
     super()
-    this.startLogin.bind(this)
+    // this.startLogin.bind(this)
   }
-  startLogin () {
-    fetch('https://github.com/login/oauth/authorize?client_id=5a57bd199dea74e2f36f', {
-      headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3000/',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Headers': 'X-Accept-Charset,X-Accept,Content-Type,Credentials'
-        //   client_id: '5a57bd199dea74e2f36f'
-        // },
-        // mode: 'no-cors'
-      }
-    })
-      .then(res => console.log(res))
-  }
+  // startLogin () {
+  //   fetch('https://github.com/login/oauth/authorize?client_id=5a57bd199dea74e2f36f'
+  //     // headers: {
+  //     //   'Access-Control-Allow-Origin': 'http://localhost:3000/',
+  //     //   'Access-Control-Allow-Methods': 'GET',
+  //     //   'Access-Control-Allow-Credentials': 'true',
+  //     //   'Access-Control-Allow-Headers': 'X-Accept-Charset,X-Accept,Content-Type,Credentials'
+  //     //   //   client_id: '5a57bd199dea74e2f36f'
+  //     //   // },
+  //     //   // mode: 'no-cors'
+  //     // }
+  //   )
+  //     .then(res => console.log(res))
+  // }
 
   render () {
     return (
       <div>
-        <a className='button' onClick={this.startLogin}>
+        <a className='button' href='https://github.com/login/oauth/authorize?client_id=5a57bd199dea74e2f36f'>
           <span className='icon'>
             <i className='fab fa-github' />
           </span>
